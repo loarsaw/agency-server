6155f2bfa6a5892c2020e07e072c8ffb1c9763a2
loarsaw <amanahmed1385@gmail.com>
2022-02-23 23:23:57 +0530

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,124 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a8tda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("admins"));
+app.use(fileUpload());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+client.connect((err) => {
+  const serviceCollection = client.db("Digital-Agency").collection("services");
+  const ordersCollection = client.db("Digital-Agency").collection("orders");
+  const reviewsCollection = client.db("Digital-Agency").collection("reviews");
+  const adminsCollection = client.db("Digital-Agency").collection("admins");
+
+  app.post("/addService", (req, res) => {
+    const file = req.files.file;
+    const title = req.body.title;
+    const description = req.body.description;
+    const img = file.name;
+
+    serviceCollection.insertOne({ title, description, img }).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+
+    file.mv(`${__dirname}/admins/${file.name}`, (err) => {
+      if (err) {
+        return res.status(500).send({ msg: "failed to Upload" });
+      }
+      return res.send({ name: file.name, path: `/${file.name}` });
+    });
+  });
+
+  app.post("/makeAdmin", (req, res) => {
+    const admins = req.body;
+    adminsCollection.insertOne(admins).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/services", (req, res) => {
+    serviceCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/orders/:key", (req, res) => {
+    serviceCollection
+      .find({ _id: ObjectId(req.params.key) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  app.post("/addOrder", (req, res) => {
+    const register = req.body;
+    ordersCollection.insertOne(register).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/serviceList", (req, res) => {
+    ordersCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.post("/addReview", (req, res) => {
+    const review = req.body;
+    reviewsCollection.insertOne(review).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/reviews", (req, res) => {
+    reviewsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/allOrders", (req, res) => {
+    ordersCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.post("/isAdmin", (req, res) => {
+    const email = req.body.email;
+    adminsCollection.find({ email: email }).toArray((err, doctors) => {
+      res.send(doctors.length > 0);
+    });
+  });
+
+  app.patch("/update/:id", (req, res) => {
+    ordersCollection
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $set: { status: req.body.status },
+        }
+      )
+      .then((result) => {
+        res.send(result.modifiedCount > 0);
+      });
+  });
 });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Backend Server...!!!!!");
+});
+
+app.listen(process.env.PORT || 4000);
